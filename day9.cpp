0c8a7d539208f662c120e25e36155acae9923f21
Sathya pramod <sathyapramod12@gmail.com>
2017-10-16 12:34:51 +0530

    Create day9.cpp

---
+++ day9.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int factorial(int n);
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int n;
+   cin>>n;
+  cout<<factorial(n)<<endl;
+    return 0;
+}
+
+int factorial(int n)
+  {
+  if(n!=1){
+    return n*factorial(n-1);  
+  }
+  return n;
+}
